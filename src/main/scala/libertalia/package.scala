45101696d07bb7c71d1bd329954f9d0cd9f0772c
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2016-10-22 03:39:36 +0300

    Configurable text file editor

--- src/main/scala/libertalia/package.scala
+++ src/main/scala/libertalia/package.scala
@@ -1,4 +1,26 @@
+import scala.collection.JavaConversions._
+
+import java.io.File
+import org.apache.commons.io.FileUtils
+
 package object libertalia {
   type ProcessCmd = PartialFunction[List[String], String]
-  val config = Config("/Applications/MacDown.app/Contents/MacOS/MacDown")
+  val config = {
+    val homeDir    = new File(System.getProperty("user.home"))
+    val configFile = new java.io.File(homeDir, ".libertalia")
+
+    if (!configFile.exists) {
+      println(s"Please configure a text file editor at ${configFile.getAbsolutePath}")
+      FileUtils.write(configFile, "editor=open")
+    }
+
+    val cfgMap: Map[String, String] = FileUtils.readLines(configFile)
+      .toList
+      .filter(!_.isEmpty)
+      .flatMap { _.split('=').toList match {
+        case k :: v :: Nil => Some(k -> v)
+        case _ => None
+      }}.toMap
+    Config(editorPath = cfgMap("editor"))
+  }
 }
